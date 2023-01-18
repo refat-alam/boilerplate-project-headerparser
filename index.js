90a499797cc8af0d24ac236297881a5437d89f4e
Refat Alam <mrefatalam1@gmail.com>
2023-01-18 12:13:16 +0600

    init

--- index.js
+++ index.js
@@ -24,6 +24,19 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', function(req, res){
+  // res.send(req.headers)
+  const ipaddress = req.headers.host
+  const language = req.headers["accept-language"]
+  const software = req.headers["user-agent"]
+
+  res.json({
+    ipaddress,
+    language,
+    software
+  })
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
